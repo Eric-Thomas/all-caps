f5b8a113bcc5645dc243e7146aa70b3c41c10f54
Eric Thomas <ethomasosu@gmail.com>
2022-12-03 20:58:42 -0500

    fetch token

--- all_caps.py
+++ all_caps.py
@@ -2,18 +2,18 @@ import requests
 import os
 import json
 
-ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 SONGS_FILE = "saved_songs.json"
 
 
 def main():
-    write_saved_songs()
+    access_token = get_access_token()
+    write_saved_songs(access_token)
     print_all_caps_songs()
 
 
-def write_saved_songs():
+def write_saved_songs(access_token):
     with open(SONGS_FILE, "w") as songs_file:
-        headers = {"Authorization": f"Bearer {ACCESS_TOKEN}", "Content-Type": "application/json"}
+        headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
         saved_songs = {}
         url = "https://api.spotify.com/v1/me/tracks?limit=50"
         while url:
@@ -30,8 +30,12 @@ def print_all_caps_songs():
     with open(SONGS_FILE, "r") as songs_file:
         songs = json.load(songs_file)
         for name, artist in songs.values():
-            if name.isupper():
+            if name.isupper() and not any(char.isdigit() for char in name):
                 print(f"{name} - {artist}")
+
+def get_access_token():
+    with open("token.json", "r") as token_file:
+        return json.load(token_file)['access_token']
 
 if __name__ == "__main__":
     main()
